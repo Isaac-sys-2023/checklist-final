13a7c9abc19a7b6450a86f87b70667fade1dc541
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-08-15 01:18:39 -0400

    feat: Add an initial Home page

--- checklist-frontend/src/app/page.tsx
+++ checklist-frontend/src/app/page.tsx
@@ -3,12 +3,14 @@ import { Task } from "@/types/Tasks";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import TaskModal from "@/components/TaskModal/TaskModal";
+import MultiTaskModal from "@/components/MultiTaskModal/MultiTaskModal";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [showModal, setShowModal] = useState(false);
+  const [showMultiModal, setShowMultiModal] = useState(false);
 
   useEffect(() => {
     try {
@@ -49,6 +51,7 @@ export default function Home() {
       <h1>Tus tareas de hoy</h1>
       
       <button onClick={() => setShowModal(true)} style={{marginBottom: '1rem'}}>New Task +</button>
+      <button onClick={() => setShowMultiModal(true)} style={{marginBottom: '1rem'}}>Multi Tasks with JSON</button>
 
       <div>
         {tasks.filter((task: Task) => task.day && task.day === new Date().toISOString().split("T")[0]).map((task: Task) => (
@@ -65,6 +68,15 @@ export default function Home() {
           <div style={{background: '#222', padding: '2rem', borderRadius: '8px', minWidth: '300px'}}>
             <TaskModal />
             <button onClick={() => setShowModal(false)} style={{marginTop: '1rem'}}>Cerrar</button>
+          </div>
+        </div>
+      )}
+
+      {showMultiModal && (
+        <div style={{position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', background: 'rgba(0,0,0,0.5)', display: 'flex', alignItems: 'center', justifyContent: 'center', zIndex: 1000}}>
+          <div style={{background: '#222', padding: '2rem', borderRadius: '8px', minWidth: '300px'}}>
+            <MultiTaskModal />
+            <button onClick={() => setShowMultiModal(false)} style={{marginTop: '1rem'}}>Cerrar</button>
           </div>
         </div>
       )}
