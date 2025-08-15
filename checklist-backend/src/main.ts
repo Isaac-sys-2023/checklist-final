49c8bf697d398de549cc05573c369e5a31d57aab
Isaac-sys-2023 <gutierrezhuarachiisaac@gmail.com>
2025-08-15 15:55:38 -0400

    feat: Implement a CORS control and his permissions

--- checklist-backend/src/main.ts
+++ checklist-backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // o ['http://localhost:3001', 'http://otro-dominio']
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
